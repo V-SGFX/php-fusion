52996f82113774a5d1d2d06311eae4e4eac500c5
V-SGFX <m.zielony@gmail.com>
2025-05-21 11:45:51 +0200

    Update theme_settings.php

--- theme_settings.php
+++ theme_settings.php
@@ -1,6 +1,8 @@
 <?php
-// Optional settings for abc-linuxu theme
-$theme_settings = array(
-    "show_logo" => true,
-);
-?>
+defined('IN_FUSION') or die();
+
+$theme_settings = [
+    'logo' => THEME.'images/logo.png',
+    'theme_version' => '1.0.0',
+    'theme_author' => 'xAI',
+];
