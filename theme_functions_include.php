0ae0957c431616842e8972d3d293629d5df84289
V-SGFX <m.zielony@gmail.com>
2025-05-21 08:19:50 +0200

    Add files via upload

---
+++ theme_functions_include.php
@@ -0,0 +1,6 @@
+<?php
+// Optional functions for abc-linuxu theme
+function render_header($header_content) {
+    echo "<header>{$header_content}</header>";
+}
+?>
---
+++ theme_settings.php
@@ -0,0 +1,6 @@
+<?php
+// Optional settings for abc-linuxu theme
+$theme_settings = array(
+    "show_logo" => true,
+);
+?>
