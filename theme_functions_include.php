b956045d0b9dd04b7bb1432fdb446c9b47528e9a
V-SGFX <m.zielony@gmail.com>
2025-05-21 11:45:39 +0200

    Update theme_functions_include.php

--- theme_functions_include.php
+++ theme_functions_include.php
@@ -1,6 +1,10 @@
 <?php
-// Optional functions for abc-linuxu theme
-function render_header($header_content) {
-    echo "<header>{$header_content}</header>";
+defined('IN_FUSION') or die();
+
+function theme_styles() {
+    add_to_head('<link rel="stylesheet" href="'.THEME.'css/bootstrap.min.css" type="text/css" />');
+    add_to_head('<link rel="stylesheet" href="'.THEME.'styles.css" type="text/css" />');
+    add_to_head('<script src="'.THEME.'js/bootstrap.bundle.min.js"></script>');
 }
-?>
+
+add_handler('theme_styles');
